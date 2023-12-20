5002bdea70da0f4c5af086be0fb137f5869d9f49
maxdemaio <maxdman321@gmail.com>
2023-12-20 11:22:32 -0500

    start cooking header/nav

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'body': ['"Source Sans Pro"'],
+			'medula': ['"Medula One"'],
+			'neuton': ['"Neuton"']
+		},
+		extend: {
+			colors: {
+				'primary': '#96BD9D',
+				'light-1': '#F2F1EB',
+				'light-2': '#EEE7DA'
+			},
+		},
+	},
+	plugins: [],
+}
